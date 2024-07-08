4ec27b21dc03ae6d8d875c4a04e1ef927c63c462
wifzw <wifzg0d@gmail.com>
2024-07-08 17:11:23 -0300

    fix: ready deployment

--- knexfile.ts
+++ knexfile.ts
@@ -5,7 +5,7 @@ import { Knex } from 'knex';
 
 const config: Knex.Config = {
   client: 'pg',
-  connection: process.env.POSTGRES_PRISMA_URL,
+  connection: process.env.POSTGRES_URL + (process.env.NODE_ENV === 'development' ? '' : '?sslmode=require'),
   migrations: {
     extension: 'ts',
   },
